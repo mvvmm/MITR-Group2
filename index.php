c59eecca2a9af1b0399c21903f8194c9da058ebe
Michael Morrison <morrim5@rpi.edu>
2019-03-23 16:10:32 -0400

    Updated index to temporarily link to different files until we get a real index

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-index
+<!doctype html>
+<html>
+<head>
+  <?php include 'style.php'?>
+  <meta charset="utf-8">
+  <title>JANRenovation</title>
+</head>
+<body>
+<h1>Index</h1>
+<a class="btn btn-primary" href="create_account.php" role="button">Create Account</a>
+<a class="btn btn-primary" href="edit_account.php" role="button">Edit Account</a>
+
+<?php
+if(isset($_COOKIE['JAN-SESSION'])){
+  echo "<a class='btn btn-primary' href='controllers/logout_controller.php'>Logout</a>";
+}else{
+  echo "<a class='btn btn-primary' href='login.php'>Login</a>";
+}
+?>
+
+</body>
+</html>
