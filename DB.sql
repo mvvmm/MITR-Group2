5c7da3f2ff12a47c7eb53c59e444462f8007a192
Michael Morrison <morrim5@rpi.edu>
2019-03-23 16:10:50 -0400

    Updated SQL to add session table and make emails unique

--- DB.sql
+++ DB.sql
@@ -9,6 +9,7 @@ CREATE TABLE IF NOT EXISTS `users`(
 	`phone` varchar(10),
 	`email` varchar(50) NOT NULL,
 	`password` varchar(255) NOT NULL,
+	UNIQUE (email),
 	PRIMARY KEY (`uid`)
 );
 CREATE TABLE IF NOT EXISTS `projects`(
@@ -26,12 +27,22 @@ CREATE TABLE IF NOT EXISTS `relations`(
 	FOREIGN KEY (uid) REFERENCES users (uid),
 	FOREIGN KEY (pid) REFERENCES projects (pid)
 );
+
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `sessionID` varchar(1000) NOT NULL,
+  `uid` int NOT NULL,
+  `expiration` DATETIME,
+  PRIMARY KEY (`sessionID`(191)),
+	FOREIGN KEY (uid) REFERENCES users (uid)
+);
+
 INSERT INTO `users` VALUES
 (1,'Bob','Marley','worker','1234567890','bob@marley.com','123'),
 (2,'Joe','Smo','worker','1234567890','joe@smo.com','321'),
 (3,'Jaryl','Ng','owner','1234567890','jaryl@ng.com','987'),
 (4,'John','Doe','admin','1234567890','john@doe.com','423'),
 (5,'Moe','Yas','admin','1234567890','moe@yas.com','324');
+
 INSERT INTO `projects` VALUES
 (1,'123 2nd street','Manhattan','2019-1-19','2018-1-20'),
 (2,'244 Vermont St.','Bronx','2019-04-19','2019-05-03'),
