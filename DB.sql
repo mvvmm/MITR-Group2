3725e2a4ea9a384248d2cd25afa43b5006af76f3
Michael Morrison <morrim5@rpi.edu>
2019-03-23 16:14:30 -0400

    Deleted dummy data, you can make your own now

--- DB.sql
+++ DB.sql
@@ -35,24 +35,3 @@ CREATE TABLE IF NOT EXISTS `sessions` (
   PRIMARY KEY (`sessionID`(191)),
 	FOREIGN KEY (uid) REFERENCES users (uid)
 );
-
-INSERT INTO `users` VALUES
-(1,'Bob','Marley','worker','1234567890','bob@marley.com','123'),
-(2,'Joe','Smo','worker','1234567890','joe@smo.com','321'),
-(3,'Jaryl','Ng','owner','1234567890','jaryl@ng.com','987'),
-(4,'John','Doe','admin','1234567890','john@doe.com','423'),
-(5,'Moe','Yas','admin','1234567890','moe@yas.com','324');
-
-INSERT INTO `projects` VALUES
-(1,'123 2nd street','Manhattan','2019-1-19','2018-1-20'),
-(2,'244 Vermont St.','Bronx','2019-04-19','2019-05-03'),
-(3,'520 Ketch Harbour Drive  ','Brooklyn','2019-05-22','2019-06-03'),
-(4,'7472 Hill Court','Queens','2019-04-20','2019-05-20'),
-(5,'987 Court Court','Staten Island','2019-08-19','2019-10-03');
-
-INSERT INTO `relations` VALUES
-(1,2,'2019-1-19'),
-(2,2,'2019-04-19'),
-(2,3,'2019-05-22'),
-(2,4,'2019-05-20'),
-(1,5,'2019-08-19');
