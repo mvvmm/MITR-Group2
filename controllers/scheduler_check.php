d9a0fcdc35ead2128a7f4f93afddcc42bf325560
Michael Morrison <morrim5@rpi.edu>
2019-03-24 14:51:46 -0400

    Added admin and scheduler checks

---
+++ controllers/scheduler_check.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'functions.php';
+$privilege = getPrivilege();
+if ($privilege != 'scheduler'){
+  echo "<script type='text/javascript'>
+          alert('You do not have permission to access this page.');
+          window.location.replace('./index.php');
+        </script>";
+  exit();
+}
+?>
