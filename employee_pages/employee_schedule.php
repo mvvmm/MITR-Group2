d00a14d0be47fabddba2e9b88f0cac93f40b94d9
natefbennett <32677820+natefbennett@users.noreply.github.com>
2019-03-29 20:40:51 -0400

    Added Schedule Grid

--- employee_pages/employee_schedule.php
+++ employee_pages/employee_schedule.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,16 +5,32 @@
     <title>Worker Portal</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+
+
 </head>
 
 <body>
 
     <div class="container">
 
-    <?php include("./navbar.php"); ?> <!-- include navbar file -->
+        <?php include("./navbar.php"); ?>
+        <!-- include navbar file -->
 
         <div class="sub-container" style="padding-left:15px; padding-right:15px;">
             <div class="row" style="padding-bottom: 15px;">
@@ -26,44 +41,161 @@
 
 
             <div class="row">
-                <div class="col-sm-12">
-                    <table class="table">
+                <div class="col-md-3">
+                    <table class="table table-bordered">
                         <thead>
                             <tr>
-                                <th scope="col">Date</th>
-                                <th scope="col">Day</th>
-                                <th scope="col">Start Time</th>
-                                <th scope="col">End Time</th>
-                                <th scope="col">Location</th>
+                                <th scope="col-sm-2">Sunday</th>
                             </tr>
                         </thead>
                         <tbody>
                             <tr>
-                                <th scope="row">3/11/2019</th>
-                                <td>Monday</td>
-                                <td>9:00 AM</td>
-                                <td>5:00 PM</td>
-                                <td>256 4th Street</td>
+                                <td scope="row">test</td>
                             </tr>
                             <tr>
-                                <th scope="row">3/12/2019</th>
-                                <td>Tuesday</td>
-                                <td>9:00 AM</td>
-                                <td>5:00 PM</td>
-                                <td>256 4th Street</td>
+                                <td scope="row">test</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-md-3">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th scope="col-sm">Sunday</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td scope="row">test</td>
                             </tr>
                             <tr>
-                                <th scope="row">3/13/2019</th>
-                                <td>Wednesday</td>
-                                <td>9:00 AM</td>
-                                <td>5:00 PM</td>
-                                <td>256 4th Street</td>
+                                <td scope="row">test</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-md-3">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th scope="col-sm">Sunday</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td scope="row">test</td>
+                            </tr>
+                            <tr>
+                                <td scope="row">test</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-md-3">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th scope="col-sm">Sunday</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td scope="row">test</td>
+                            </tr>
+                            <tr>
+                                <td scope="row">test</td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
             </div>
+            <div class="row">
+                <div class="col-md-3">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th scope="col-sm-2">Sunday</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td scope="row">test</td>
+                            </tr>
+                            <tr>
+                                <td scope="row">test</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-md-3">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th scope="col-sm">Sunday</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td scope="row">test</td>
+                            </tr>
+                            <tr>
+                                <td scope="row">test</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="col-md-3">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th scope="col-sm">Sunday</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td scope="row">test</td>
+                            </tr>
+                            <tr>
+                                <td scope="row">test</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <!--
+                <div class="col-sm-12">
+                    <table class="table table-bordered table-responsive">
+                        <thead>
+                            <tr>
+                                <th scope="col" style="width: 14.2857143%; text-align: center">Sunday</th>
+                                <th scope="col" style="width: 14.2857143%">Monday</th>
+                                <th scope="col" style="width: 14.2857143%">Tuesday</th>
+                                <th scope="col" style="width: 14.2857143%">Wedsday</th>
+                                <th scope="col" style="width: 14.2857143%">Thursday</th>
+                                <th scope="col" style="width: 14.2857143%">Friday</th>
+                                <th scope="col" style="width: 14.2857143%">Saturday</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <div class="container-fluid">
+                                    235 4th St.
+                                    </div>
+                                    <div class="container-fluid">
+                                    9:00 AM - 5:00 PM
+                                    </div>
+
+                                
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            -->
         </div>
+    </div>
     </div>
 
 </body>
