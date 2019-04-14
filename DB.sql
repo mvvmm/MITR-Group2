7ff642073c63e69b87703b746d985177a7b50914
Achau9 <achau@afsenyc.org>
2019-04-14 13:32:52 -0400

    Included timesheet table and change datetime type to date for db.sql

--- DB.sql
+++ DB.sql
@@ -9,34 +9,25 @@ CREATE TABLE IF NOT EXISTS `users`(
 	`phone` varchar(10),
 	`email` varchar(50) NOT NULL,
 	`password` varchar(255) NOT NULL,
-	`approved` tinyint(1),
-	UNIQUE (`email`),
+	UNIQUE (email),
 	PRIMARY KEY (`uid`)
 );
 CREATE TABLE IF NOT EXISTS `projects`(
 	`pid` int(11) NOT NULL AUTO_INCREMENT,
 	`address` varchar(15) NOT NULL,
 	`borough` varchar(15) NOT NULL,
-	`start` DATETIME,
-	`end` DATETIME,
+	`start` DATE,
+	`end` DATE,
 	PRIMARY KEY (`pid`)
 );
 CREATE TABLE IF NOT EXISTS `relations`(
 	`uid` int(11) NOT NULL,
 	`pid` int(11) NOT NULL,
-	`date` DATETIME,
+	`date` DATE,
 	FOREIGN KEY (uid) REFERENCES users (uid),
 	FOREIGN KEY (pid) REFERENCES projects (pid)
 );
-CREATE TABLE IF NOT EXISTS `timesheet`(
-	`uid` int(11) NOT NULL,
-	`pid` int(11) NOT NULL,
-	`starttime` DATETIME,
-	`endtime` DATETIME,
-	
-	FOREIGN KEY (uid) REFERENCES users (uid),
-	FOREIGN KEY (pid) REFERENCES projects (pid)
-);
+
 CREATE TABLE IF NOT EXISTS `sessions` (
   `sessionID` varchar(1000) NOT NULL,
   `uid` int NOT NULL,
@@ -44,4 +35,11 @@ CREATE TABLE IF NOT EXISTS `sessions` (
   PRIMARY KEY (`sessionID`(191)),
 	FOREIGN KEY (uid) REFERENCES users (uid)
 );
-
+CREATE TABLE IF NOT EXISTS `timesheet`(
+	`uid` int(11) NOT NULL,
+	`pid` int(11) NOT NULL,
+	`starttime` DATETIME,
+	`endtime` DATETIME,
+	FOREIGN KEY (uid) REFERENCES users (uid),
+	FOREIGN KEY (pid) REFERENCES projects (pid)
+);
