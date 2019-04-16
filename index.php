9d1c345acfb3c46e0a4258730bfacebe8be5aab3
Achau9 <achau@afsenyc.org>
2019-04-16 11:07:07 -0400

    Initial changes to dashboard

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php require_once 'controllers/functions.php'?>
 <!doctype html>
 <html>
 <head>
@@ -8,5 +9,18 @@
 <body>
 <?php include 'navbar.php'; ?>
 <h1>Index</h1>
+ <?php
+	$user_type = getPrivilege();
+	if($user_type == "employee"){
+	  echo "  <div class='form-group'>";
+	  echo "      <select class='form-control' id='privilege' name='privilege'>";
+	  echo "        <option selected disabled>Privilege</option>";
+	  echo "        <option value='employee'>Employee</option>";
+	  echo "        <option value='scheduler'>Scheduler</option>";
+	  echo "        <option value='admin'>Admin</option>";
+	  echo "      </select>";
+	  echo "   </div>";
+	}
+?>
 </body>
 </html>
--- mitrdata.sql
+++ mitrdata.sql
@@ -51,18 +51,18 @@ INSERT INTO `users` VALUES
 (4,'John','Doe','admin','1234567890','john@doe.com','423','0'),
 (5,'Moe','Yas','admin','1234567890','moe@yas.com','324','0');
 INSERT INTO `projects` VALUES
-(1,'123 2nd street','Manhattan','2019-1-19','2018-1-20'),
-(2,'244 Vermont St.','Bronx','2019-04-19','2019-05-03'),
-(3,'520 Ketch Harbour Drive  ','Brooklyn','2019-05-22','2019-06-03'),
-(4,'7472 Hill Court','Queens','2019-04-20','2019-05-20'),
-(5,'987 Court Court','Staten Island','2019-08-19','2019-10-03');
+(1,'123 2nd street','Manhattan','2019-1-19','2018-1-20','1'),
+(2,'244 Vermont St.','Bronx','2019-04-19','2019-05-03','1'),
+(3,'520 Ketch Harbour Drive  ','Brooklyn','2019-05-22','2019-06-03','1'),
+(4,'7472 Hill Court','Queens','2019-04-20','2019-05-20','1'),
+(5,'987 Court Court','Staten Island','2019-08-19','2019-10-03','1');
 
 INSERT INTO `relations` VALUES
 (1,2,'2019-1-19'),
+(1,5,'2019-08-19'),
 (2,2,'2019-04-19'),
 (2,3,'2019-05-22'),
-(2,4,'2019-05-20'),
-(1,5,'2019-08-19');
+(2,4,'2019-05-20');
 
 INSERT INTO `timesheet` VALUES
 (1,2,'2019-1-19 12:00','2019-1-19 13:00'),
