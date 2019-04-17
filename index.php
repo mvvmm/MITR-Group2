8b03496b9ad610302efa0da4894248eb1233c0f5
Michael Morrison <morrim5@rpi.edu>
2019-04-16 20:32:27 -0400

    Adjusted_Work_Schedule page

--- index.php
+++ index.php
@@ -3,124 +3,72 @@
 <html>
 <head>
   <?php include 'style.php'?>
+  <?php include 'script.php'?>
   <meta charset="utf-8">
   <title>JANRenovation</title>
 </head>
 <body>
-<?php include 'navbar.php'; ?>
-<h1>Index</h1>
- <?php
-	$user_type = getPrivilege();
-	if($user_type == "employee"){
-		$uid = getUID();
-	  
-		$conn = dbConnect();
-		$stmt = $conn->prepare('SELECT r.date,p.address,p.borough FROM relations r, projects p WHERE r.uid = :uid AND r.pid = p.pid AND p.active = 1');
-		$stmt->bindParam(':uid', $uid);
-		$stmt->execute();
-		$result = $stmt->fetchAll();
-		// var_dump($result);
-		echo("<div class=\"sub-container\" style=\"padding-left:15px; padding-right:15px;\">
-            <div class=\"row\" style=\"padding-bottom: 15px;\">
-                <div class=\"col-sm-12\">
-                    <h3>Your Work Schedule</h3>
-                </div>
-            </div>");
+  <?php include 'navbar.php';
+  $user_type = getPrivilege();
+  if($user_type == "employee"){?>
+
+  <div class="sub-container" style="padding-left:15px; padding-right:15px;">
+    <div class="row" style="padding-bottom: 15px;">
+      <div class="col-sm-12">
+        <h1 class="pt-5">Your Work Schedule</h1>
+      </div>
+    </div>
+
+    <div class="row">
+
+      <div class="col-md-3">
+        <table class="table table-bordered" id="Monday">
+          <thead class='thead-dark'>
+            <tr><th scope="col-sm-1">Monday</th></tr>
+          </thead>
+          <tbody id="Monday"></tbody>
+        </table>
+      </div>
+
+      <div class="col-md-3">
+        <table class="table table-bordered" id="Tuesday">
+          <thead class='thead-dark'>
+            <tr><th scope="col-sm-1">Tuesday</th></tr>
+          </thead>
+          <tbody id="Tuesday"></tbody>
+        </table>
+      </div>
+
+      <div class="col-md-3">
+        <table class="table table-bordered" id="Wednesday">
+          <thead class='thead-dark'>
+            <tr><th scope="col-sm-1">Wednesday</th></tr>
+          </thead>
+          <tbody id="Wednesday"></tbody>
+        </table>
+      </div>
+
+      <div class="col-md-3">
+        <table class="table table-bordered" id="Thursday">
+          <thead class='thead-dark'>
+            <tr><th scope="col-sm-1">Thursday</th></tr>
+          </thead>
+          <tbody id="Thursday"></tbody>
+        </table>
+      </div>
+
+      <div class="col-md-3">
+        <table class="table table-bordered" id="Friday">
+          <thead class='thead-dark'>
+            <tr><th scope="col-sm-1">Friday</th></tr>
+          </thead>
+          <tbody id="Friday"></tbody>
+        </table>
+      </div>
+    </div>
 
 
-         echo("<div class=\"row\">
-                <div class=\"col-md-3\">
-                    <table class=\"table table-bordered\" id = \"Monday\">
-                        <thead>
-                            <tr>
-                                <th scope=\"col-sm-1\">Monday</th>
-                            </tr>
-                        </thead>
-                        <tbody>");
-        foreach($result as $value){
-
-        	$dayofweek = date("l",strtotime($value["date"]));
-        	
-        	if($dayofweek=="Monday"){
-        		echo("<tr><td scope=\"row\">".$value["address"].", ".$value["borough"]."</td></tr>");
-        	}
-        }
-        echo("			</tbody>
-                    </table>
-                </div>
-                <div class=\"col-md-3\">
-                    <table class=\"table table-bordered\" id = \"Tuesday\">
-                        <thead>
-                            <tr>
-                                <th scope=\"col-sm\">Tuesday</th>
-                            </tr>
-                        </thead>
-                        <tbody>");
-
-        foreach($result as $value){
-        	$dayofweek = date("l",strtotime($value["date"]));
-        	if($dayofweek=="Tuesday"){
-        		echo("<tr><td scope=\"row\">".$value["address"].", ".$value["borough"]."</td></tr>");
-        	}
-        }                   
-        echo("         </tbody>
-                    </table>
-                </div>
-                <div class=\"col-md-3\">
-                    <table class=\"table table-bordered\" id = \"Wednesday\">
-                        <thead>
-                            <tr>
-                                <th scope=\"col-sm\">Wednesday</th>
-                            </tr>
-                        </thead>
-                        <tbody>");
-        foreach($result as $value){
-        	$dayofweek = date("l",strtotime($value["date"]));
-        	if($dayofweek=="Wednesday"){
-        		echo("<tr><td scope=\"row\">".$value["address"].", ".$value["borough"]."</td></tr>");
-        	}
-        }           
-        echo("                </tbody>
-                    </table>
-                </div>
-                <div class=\"col-md-3\">
-                    <table class=\"table table-bordered\"id = \"Thursday\">
-                        <thead>
-                            <tr>
-                                <th scope=\"col-sm\">Thursday</th>
-                            </tr>
-                        </thead>
-                        <tbody>");
-        foreach($result as $value){
-        	$dayofweek = date("l",strtotime($value["date"]));
-        	if($dayofweek=="Thursday"){
-        		echo("<tr><td scope=\"row\">".$value["address"].", ".$value["borough"]."</td></tr>");
-        	}
-        }                 
-        echo("                </tbody>
-                    </table>
-                </div>
-                <div class=\"col-md-3\">
-                    <table class=\"table table-bordered\" id = \"Friday\">
-                        <thead>
-                            <tr>
-                                <th scope=\"col-sm-1\">Friday</th>
-                            </tr>
-                        </thead>
-                        <tbody>");
-        foreach($result as $value){
-        	$dayofweek = date("l",strtotime($value["date"]));
-        	if($dayofweek=="Friday"){
-        		echo("<tr><td scope=\"row\">".$value["address"].", ".$value["borough"]."</td></tr>");
-        	}
-        }                
-        echo("               </tbody>
-                    </table>
-                </div>
-            </div>");
-	
-	  
-	}
-?>
+    <?php generateUserSchedule();
+  }?>
 </body>
 </html>
