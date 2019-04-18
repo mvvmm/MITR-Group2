9d36cb2128c9ce5e7cfa0c4a0634ca01329958ab
Michael Morrison <morrim5@rpi.edu>
2019-04-17 20:47:41 -0400

    Added calendars to create_project and checked to make sure user wasn't trying to add a duplicate project into the database

--- controllers/create_project_controller.php
+++ controllers/create_project_controller.php
@@ -1,29 +1,38 @@
 <?php
 include_once "db_connector.php";
-if(isset($_POST['address']) && $_POST['borough'] && $_POST['startDateInput'] && $_POST['startTimeInput'] && $_POST['endDateInput'] && $_POST['endTimeInput']) {
+if(isset($_POST['address']) && $_POST['borough'] && $_POST['startDate'] && $_POST['endDate']) {
     $address = $_POST['address'];
     $borough = $_POST['borough'];
 
-    $startDate = $_POST['startDateInput'];
-    $startTime = date("H:i:s", strtotime($_POST['startTimeInput']));
-    $startDatetime = $startDate . " " . $startTime;
+    $startDate = DateTime::createFromFormat('m/d/Y', $_POST['startDate']."00:00:00");
+    $endDate = DateTime::createFromFormat('m/d/Y', $_POST['endDate']."00:00:00");
 
-    $endDate = $_POST['endDateInput'];
-    $endTime = date("H:i:s", strtotime($_POST['endTimeInput']));
-    $endDatetime = $endDate . " " . $endTime;
+    if ($startDate == false || $endDate == false){
+      echo "<script type='text/javascript'>alert('One or both of the dates and times you entered isn't the correct format. Try again using the calendar dropdown.');
+      window.location.replace(\" ../create_project.php \");
+      </script>";
+    }
 
-    $active = 1;
-
+    // Check to see if a project already exists with this address
     $conn = dbConnect();
+    $stmt = $conn->prepare('SELECT * FROM projects where address=:address');
+    $stmt->bindParam(':address',$address);
+    $stmt->execute();
+    $stmt->fetchAll();
+    if($stmt){
+      echo "<script type='text/javascript'>alert('A project with this address already exists.');
+      window.location.replace(\" ../create_project.php \");
+      </script>";
+    }
+
     $stmt = $conn->prepare('INSERT INTO projects (address,borough,starttime,endtime,active)
-      VALUES (:address, :borough, :starttime, :endtime,:active)');
+      VALUES (:address, :borough, :starttime, :endtime,1)');
     $stmt->bindParam(':address',$address);
     $stmt->bindParam(':borough',$borough);
-    $stmt->bindParam(':starttime',$startDatetime);
-    $stmt->bindParam(':endtime',$endDatetime);
-    $stmt->bindParam(':active', $active);
+    $stmt->bindParam(':starttime',$startDate);
+    $stmt->bindParam(':endtime',$endDate);
     $stmt->execute();
 } else {
-    die("ERROR: You must put something for all inputs.");
+    die("ERROR");
 }
 ?>
