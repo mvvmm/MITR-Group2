ed47acb8c7fcc43b028fd2fdf263c1235dabc897
justinchen673 <justinchenchen673@gmail.com>
2019-04-22 10:50:46 -0400

    Spelling fix

--- controllers/manager_check.php
+++ controllers/manager_check.php
@@ -3,7 +3,7 @@ require_once 'functions.php';
 $privilege = getPrivilege();
 if ($privilege != 'admin' && $privilege != 'scheduler'){
   echo "<script type='text/javascript'>
-          alert('You do nottttttt have permission to access this page.');
+          alert('You do not have permission to access this page.');
           window.location.replace('./index.php');
         </script>";
   exit();
